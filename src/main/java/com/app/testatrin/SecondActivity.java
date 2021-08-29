432cea2216e1b059edc8cb82ed12af9cd3954b28
pouya16 <pouya16@gmail.com>
2021-08-29 11:25:04 +0430

    1400/06/07 finish ui

--- src/main/java/com/app/testatrin/SecondActivity.java
+++ src/main/java/com/app/testatrin/SecondActivity.java
@@ -1,14 +1,14 @@
 package com.app.testatrin;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 }
